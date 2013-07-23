5bdadce31910b0f851b6bf861f57bddf04da74c1
Petr Trofimov <petrofimov@yandex.ru>
2013-07-23 23:00:24 +0300

    server stats in groups

--- lib/include.php
+++ lib/include.php
@@ -50,6 +50,66 @@ class Console
         return $stats;
     }
 
+    public function getServerStatsGroups()
+    {
+        return array(
+            'binlog' => array(
+                'binlog-current-index' => 'the index of the current binlog file being written to. If binlog is not active this value will be 0',
+                'binlog-max-size' => 'the maximum size in bytes a binlog file is allowed to get before a new binlog file is opened',
+                'binlog-oldest-index' => 'the index of the oldest binlog file needed to store the current jobs',
+                'binlog-records-migrated' => 'the cumulative number of records written as part of compaction',
+                'binlog-records-written' => 'the cumulative number of records written to the binlog',
+            ),
+            'cmd' => array(
+                'cmd-bury' => 'the cumulative number of bury commands',
+                'cmd-delete' => 'the cumulative number of delete commands',
+                'cmd-ignore' => 'the cumulative number of ignore commands',
+                'cmd-kick' => 'the cumulative number of kick commands',
+                'cmd-list-tube-used' => 'the cumulative number of list-tube-used commands',
+                'cmd-list-tubes' => 'the cumulative number of list-tubes commands',
+                'cmd-list-tubes-watched' => 'the cumulative number of list-tubes-watched commands',
+                'cmd-pause-tube' => 'the cumulative number of pause-tube commands',
+                'cmd-peek' => 'the cumulative number of peek commands',
+                'cmd-peek-buried' => 'the cumulative number of peek-buried commands',
+                'cmd-peek-delayed' => 'the cumulative number of peek-delayed commands',
+                'cmd-peek-ready' => 'the cumulative number of peek-ready commands',
+                'cmd-put' => 'the cumulative number of put commands',
+                'cmd-release' => 'the cumulative number of release commands',
+                'cmd-reserve' => 'the cumulative number of reserve commands',
+                'cmd-stats' => 'the cumulative number of stats commands',
+                'cmd-stats-job' => 'the cumulative number of stats-job commands',
+                'cmd-stats-tube' => 'the cumulative number of stats-tube commands',
+                'cmd-use' => 'the cumulative number of use commands',
+                'cmd-watch' => 'the cumulative number of watch commands',
+            ),
+            'current' => array(
+                'current-connections' => 'the number of currently open connections',
+                'current-jobs-buried' => 'the number of buried jobs',
+                'current-jobs-delayed' => 'the number of delayed jobs',
+                'current-jobs-ready' => 'the number of jobs in the ready queue',
+                'current-jobs-reserved' => 'the number of jobs reserved by all clients',
+                'current-jobs-urgent' => 'the number of ready jobs with priority < 1024',
+                'current-producers' => 'the number of open connections that have each issued at least one put command',
+                'current-tubes' => 'the number of currently-existing tubes',
+                'current-waiting' => 'the number of open connections that have issued a reserve command but not yet received a response',
+                'current-workers' => 'the number of open connections that have each issued at least one reserve command',
+            ),
+            'other' => array(
+                'hostname' => 'the hostname of the machine as determined by uname',
+                'id' => 'a random id string for this server process, generated when each beanstalkd process starts',
+                'job-timeouts' => 'the cumulative count of times a job has timed out',
+                'max-job-size' => 'the maximum number of bytes in a job',
+                'pid' => 'the process id of the server',
+                'rusage-stime' => 'the cumulative system CPU time of this process in seconds and microseconds',
+                'rusage-utime' => 'the cumulative user CPU time of this process in seconds and microseconds',
+                'total-connections' => 'the cumulative count of connections',
+                'total-jobs' => 'the cumulative count of jobs created',
+                'uptime' => 'the number of seconds since this server process started running',
+                'version' => 'the version string of the server',
+            ),
+        );
+    }
+
     protected function __init()
     {
         global $server, $action, $count, $tube, $config;
--- lib/tpl/serversList.php
+++ lib/tpl/serversList.php
@@ -97,17 +97,30 @@ if (!empty($_COOKIE['filter'])) {
     </div>
     <div class="modal-body">
         <form>
-            <?php foreach($console->getServerStats(reset($servers)) as $key => $item):?>
-                <div class="control-group">
-                    <div class="controls">
-                        <label class="checkbox">
-                            <input type="checkbox" name="<?php echo $key?>" <?php if(in_array($key, $visible)) echo 'checked="checked"'?>>
-                            <b><?php echo $key?></b>
-                            <br /><?php echo $item['description']?>
-                        </label>
+            <div class="tabbable">
+                <ul class="nav nav-tabs">
+                    <?php $i=0; foreach($console->getServerStatsGroups() as $groupName => $fields): $i++;?>
+                        <li <?php if($i==1) echo 'class="active"'?>><a href="#<?php echo $groupName?>" data-toggle="tab"><?php echo $groupName?></a></li>
+                    <?php endforeach?>
+                </ul>
+                <div class="tab-content">
+                    <?php $i=0; foreach($console->getServerStatsGroups() as $groupName => $fields): $i++;?>
+                    <div class="tab-pane <?php if($i==1) echo 'active'?>" id="<?php echo $groupName?>">
+                        <?php foreach($fields as $key => $description):?>
+                            <div class="control-group">
+                                <div class="controls">
+                                    <label class="checkbox">
+                                        <input type="checkbox" name="<?php echo $key?>" <?php if(in_array($key, $visible)) echo 'checked="checked"'?>>
+                                        <b><?php echo $key?></b>
+                                        <br /><?php echo $description?>
+                                    </label>
+                                </div>
+                            </div>
+                        <?php endforeach?>
                     </div>
+                    <?php endforeach?>
                 </div>
-            <?php endforeach?>
+            </div>
         </form>
     </div>
     <div class="modal-footer">
