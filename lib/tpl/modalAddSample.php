996d8f0161d09de516fd1ed536078d16c8130ac4
pentium10 <pentium10@gmail.com>
2014-09-04 14:30:03 +0300

    fixes #62

--- lib/tpl/modalAddSample.php
+++ lib/tpl/modalAddSample.php
@@ -1,4 +1,4 @@
-<div id="modalAddSample" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="settings-label" aria-hidden="true">
+<div id="modalAddSample" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="settings-label" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
