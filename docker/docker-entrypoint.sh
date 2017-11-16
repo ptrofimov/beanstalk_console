d7b3bc057223da596d8f2151b220310258bd3b05
Zabielski, Kamil <kamil.zabielski@outlook.com>
2017-11-16 19:57:03 +0100

    Cut `ERROR_REPORTING`
    Signed-off-by: Zabielski, Kamil <kamil.zabielski@outlook.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,9 +5,6 @@ set -e
 # Change ownership for apache happiness
 chown -R www-data:www-data "${APACHE_DOCROOT}"
 
-PHP_ERROR_REPORTING=${PHP_ERROR_REPORTING:-"E_ALL & ~E_DEPRECATED & ~E_NOTICE"}
-echo "error_reporting = $PHP_ERROR_REPORTING" >> /usr/local/lib/php.ini
-
 if [[ -n "$BEANSTALKD_HOST" ]]; then
 
   if [[ -z "$BEANSTALKD_PORT" ]]; then
